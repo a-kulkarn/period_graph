42c90a8800a2ff65e22926fd248e103c915b169b
Avinash Kulkarni <avinash.a.kulkarni@dartmouth.edu>
2020-07-16 18:04:23 -0400

    Added tests.all

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,3 @@
 
 from __init__ import *
 
-#from tests.star1 import *
-#from tests.star2 import *
-#from tests.lankystar1 import *
-#from tests.lankystar2 import *
-
---
+++ tests/all.py
@@ -0,0 +1,14 @@
+
+import tests.star1
+import tests.star2
+import tests.lankystar1
+import tests.lankystar2
+
+import tests.training1
+import tests.training2
+import tests.training3
+
+import tests.neural_network1
+import tests.neural_network2
+
+
