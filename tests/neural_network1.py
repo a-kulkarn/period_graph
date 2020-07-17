ddad7c2bcefdbb61bdf81c0057f3558770e2b0b0
Avinash Kulkarni <avinash.a.kulkarni@dartmouth.edu>
2020-07-17 12:44:17 -0400

    Fix test check.

--- tests/neural_network1.py
+++ tests/neural_network1.py
@@ -12,11 +12,11 @@ TEST_PATH = os.path.join(os.path.join(SELF_PATH, "tests", ""))
 R = PolynomialRing(QQ, 4, "xyzw")
 (x,y,z,w) = R.gens()
 
-E = [[x**4 + y**4 + z**4 + w**4,x**4 + y**4 + z**4 + z*w**3],
-     [x**4 + y**4 + z**4 + w**4,x**4 + y**4 + z**3*w + w**4],
-     [x**4 + y**4 + z**4 + w**4,x**4 + y**4 + z**4 + x*w**3],
-     [x**4 + y**4 + z**4 + w**4,x**4 + y**4 + z**4 + y*w**3],
-     [x**4 + y**4 + z**4 + w**4,x**4 + y**4 + x*z**3 + w**4]]
+E = [(x**4 + y**4 + z**4 + w**4,x**4 + y**4 + z**4 + z*w**3),
+     (x**4 + y**4 + z**4 + w**4,x**4 + y**4 + z**3*w + w**4),
+     (x**4 + y**4 + z**4 + w**4,x**4 + y**4 + z**4 + x*w**3),
+     (x**4 + y**4 + z**4 + w**4,x**4 + y**4 + z**4 + y*w**3),
+     (x**4 + y**4 + z**4 + w**4,x**4 + y**4 + x*z**3 + w**4)]
 
 # Run the training program
 sE, _, _ = nn_sort(E)
