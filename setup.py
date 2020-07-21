a5fec8243b05ece96458c4c6bda9c0440fc2971a
Avinash Kulkarni <avinash.a.kulkarni@dartmouth.edu>
2020-07-21 01:55:10 -0400

    Added python pip package setup.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+from setuptools import setup
+
+setup(name='period_graph',
+      version='0.1',
+      description='Saves and parallelizes computations of periods of (quartic) hypersurfaces',
+      url='https://github.com/a-kulkarn/period_graph.git',
+      author='Avinash Kulkarni',
+      author_email='avi.kulkarni.2.71@gmail.com',
+      license='MIT',
+      packages=['period_graph'],
+      zip_safe=False)
